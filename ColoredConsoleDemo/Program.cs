717d7d32015214acb066feb807c5aeb60e829f77
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-10-07 08:24:18 +0200

    Applying Code Cleanup.

--- ColoredConsoleDemo/Program.cs
+++ ColoredConsoleDemo/Program.cs
@@ -1,42 +1,41 @@
 using CK.Core;
 using System.Diagnostics;
 
-namespace ColoredConsoleDemo
+namespace ColoredConsoleDemo;
+
+class Program
 {
-    class Program
+    static void Main( string[] args )
     {
-        static void Main( string[] args )
+        var m = new ActivityMonitor( topic: "I'm full of colors (and I'm playing with levels and filters)." );
+
+        m.Output.RegisterClient( new ColoredActivityMonitorConsoleClient() );
+
+        m.MinimalFilter = LogFilter.Terse;
+        using( m.OpenInfo( $"Let's go: the actual filter is {m.ActualFilter}." ) )
         {
-            var m = new ActivityMonitor( topic: "I'm full of colors (and I'm playing with levels and filters)." );
+            m.Info( "In Terse filter, only Info (and above) groups and only Error or Fatal lines are logged. So you won't see this." );
 
-            m.Output.RegisterClient( new ColoredActivityMonitorConsoleClient() );
-
-            m.MinimalFilter = LogFilter.Terse;
-            using( m.OpenInfo( $"Let's go: the actual filter is {m.ActualFilter}." ) )
+            using( m.OpenError( "A whole group of error." ) )
             {
-                m.Info( "In Terse filter, only Info (and above) groups and only Error or Fatal lines are logged. So you won't see this." );
+                m.Debug( "Debug is the lowest level." );
+                m.Debug( $"In an Error or Fatal group, the minimal filter is automatically set to {m.ActualFilter} (that's why you can see this line)." );
+                m.Trace( "Trace is low level." );
+                m.Info( "Info is interesting." );
+                m.Warn( "Warning is not serious." );
+                m.Error( "Error should be investigated." );
+                m.Fatal( "Fatal ruins the process." );
 
-                using( m.OpenError( "A whole group of error." ) )
-                {
-                    m.Debug( "Debug is the lowest level." );
-                    m.Debug( $"In an Error or Fatal group, the minimal filter is automatically set to {m.ActualFilter} (that's why you can see this line)." );
-                    m.Trace( "Trace is low level." );
-                    m.Info( "Info is interesting." );
-                    m.Warn( "Warning is not serious." );
-                    m.Error( "Error should be investigated." );
-                    m.Fatal( "Fatal ruins the process." );
-
-                    m.CloseGroup( "This concludes the error (the using } close below will do nothing)." );
-                }
-                m.MinimalFilter = LogFilter.Verbose;
-                m.Info( "Verbose filter let's the Info lines be logged." );
-                m.Info( "Note that closing a group restores the minimal filter level that was set on the opening." );
+                m.CloseGroup( "This concludes the error (the using } close below will do nothing)." );
             }
-            Debug.Assert( m.ActualFilter == LogFilter.Terse );
-            m.MinimalFilter = LogFilter.Monitor;
-            m.Warn( "Using Monitor filter, only Warn, Error and Fatal lines are logged." );
-
-            System.Console.ReadLine();
+            m.MinimalFilter = LogFilter.Verbose;
+            m.Info( "Verbose filter let's the Info lines be logged." );
+            m.Info( "Note that closing a group restores the minimal filter level that was set on the opening." );
         }
+        Debug.Assert( m.ActualFilter == LogFilter.Terse );
+        m.MinimalFilter = LogFilter.Monitor;
+        m.Warn( "Using Monitor filter, only Warn, Error and Fatal lines are logged." );
+
+        System.Console.ReadLine();
     }
 }
--- MonitoringDemoApp/LoggerTestHostedServiceConfiguration.cs
+++ MonitoringDemoApp/LoggerTestHostedServiceConfiguration.cs
@@ -1,31 +1,30 @@
 using System;
 using System.Threading.Tasks;
 
-namespace MonitoringDemoApp
+namespace MonitoringDemoApp;
+
+/// <summary>
+/// Configures the <see cref="LoggerTestHostedService"/>.
+/// </summary>
+public class LoggerTestHostedServiceConfiguration
 {
-    /// <summary>
-    /// Configures the <see cref="LoggerTestHostedService"/>.
-    /// </summary>
-    public class LoggerTestHostedServiceConfiguration
+    public enum WorkingMode
     {
-        public enum WorkingMode
-        {
-            None,
-            Standard,
-            StandardWithUnobservedTaskException,
-            Tagged
-        }
-
-        /// <summary>
-        /// Gets or sets the timer duration.
-        /// </summary>
-        public TimeSpan TimerDuration { get; set; }
-
-        /// <summary>
-        /// Gets or sets the <see cref="WorkingMode"/>.
-        /// </summary>
-        public WorkingMode Mode { get; set; } = WorkingMode.Standard;
-
-        public override string ToString() => $"Options{{ TimerDuration = {TimerDuration}, Mode = {Mode} }}";
+        None,
+        Standard,
+        StandardWithUnobservedTaskException,
+        Tagged
     }
+
+    /// <summary>
+    /// Gets or sets the timer duration.
+    /// </summary>
+    public TimeSpan TimerDuration { get; set; }
+
+    /// <summary>
+    /// Gets or sets the <see cref="WorkingMode"/>.
+    /// </summary>
+    public WorkingMode Mode { get; set; } = WorkingMode.Standard;
+
+    public override string ToString() => $"Options{{ TimerDuration = {TimerDuration}, Mode = {Mode} }}";
 }
